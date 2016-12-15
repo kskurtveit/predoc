e576da88df8fbaca1bd4d2f26fde451d6d72c7b0
Kristian Skurtveit <kristian.skurtveit@uib.no>
2016-12-15 09:16:04 +0100

    serve_static_assets to serve_static files. Enable logging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,7 @@ Predoc::Application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.eager_load = false
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
@@ -12,6 +13,9 @@ Predoc::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+
+  # See everything in the log (default is :info)
+  config.log_level = :debug
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
